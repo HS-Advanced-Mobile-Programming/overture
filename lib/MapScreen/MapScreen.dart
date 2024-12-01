a17339aef8b482aa6dc502b1bf8cfe8bcb17a581
junni01kim <audwnssp@naver.com>
2024-12-01 18:47:55 +0900

    안드로이드 구글 페이지 생성

--- lib/MapScreen/MapScreen.dart
+++ lib/MapScreen/MapScreen.dart
@@ -1,15 +1,31 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
 
   @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        '지도 화면',
-        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    return Center(
+      child: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 11.0,
+        ),
       ),
     );
   }
