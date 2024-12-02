290d3e27b99aec6b8d78ab3315c309f602c191a7
junni01kim <audwnssp@naver.com>
2024-12-03 00:50:06 +0900

    UI 일부 수정

--- lib/MapScreen/TopSheet.dart
+++ lib/MapScreen/TopSheet.dart
@@ -7,12 +7,10 @@ class TopSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Color(0xFFF0F4F6),
-      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
       child: const Row(
         children: [
-          Icon(Icons.arrow_back, size: 30),
-          SizedBox(width: 10), // 아이콘과 텍스트 간격 추가
-          Icon(Icons.map_rounded, size: 35), // TODO: 추후 색상있는 것으로 변경할 것 or 이미지
+          Icon(Icons.arrow_back_ios, size: 20),
           SizedBox(width: 10), // 아이콘과 텍스트 간격 추가
           Text("지도", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
         ],
