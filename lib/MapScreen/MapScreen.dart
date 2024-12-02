3646f73b4979182b91ddf176fc0999b7f8df27c5
junni01kim <audwnssp@naver.com>
2024-12-02 22:36:23 +0900

    마커 아이콘 변경 완료

--- lib/MapScreen/MapScreen.dart
+++ lib/MapScreen/MapScreen.dart
@@ -9,6 +9,7 @@ class Schedule {
   final String title;
   final DateTime time;
   final LatLng latLng;
+  BitmapDescriptor? icon; // 아이콘 추가
 
   Schedule({
     required this.scheduleId,
@@ -16,6 +17,7 @@ class Schedule {
     required this.title,
     required this.time,
     required this.latLng,
+    this.icon,
   });
 
   @override
@@ -24,7 +26,7 @@ class Schedule {
   }
 }
 
-List<Schedule> sampleData = [
+List<Schedule> schedules = [
   Schedule(scheduleId: 1, userId: 1, title: "출국", time: DateTime(2024, 12, 1, 1, 0, 0), latLng: LatLng(37.63695556, 127.0277194)),
   Schedule(scheduleId: 2, userId: 1, title: "호텔 체크인", time: DateTime(2024, 12, 1, 3, 0, 0), latLng: LatLng(37.68695556, 127.0277194)),
   Schedule(scheduleId: 3, userId: 1, title: "파이브가이즈에서 식사", time: DateTime(2024, 12, 1, 6, 0, 0), latLng: LatLng(37.73695556, 127.0277194)),
@@ -36,6 +38,8 @@ List<Schedule> sampleData = [
   Schedule(scheduleId: 9, userId: 1, title: "귀국", time: DateTime(2024, 12, 3, 6, 0, 0), latLng: LatLng(38.03695556, 127.1277194)),
 ];
 
+// TODO: schedules로 API 주입 받을 것
+
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
 
@@ -46,8 +50,46 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   late GoogleMapController mapController;
   DateTime? selectedDate; // 선택된 날짜
+  final List<BitmapDescriptor> customIcons = []; // 사용자 정의 아이콘 리스트
 
   final LatLng _center = const LatLng(37.63695556, 127.0277194);
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCustomIcons();
+  }
+
+  // TODO: 로직 내부에서 정렬을 계속 진행하는데, 수정 필요 있음
+  // BitmapDescriptor 아이콘을 로드하여 customIcons 리스트에 추가
+  Future<void> _loadCustomIcons() async {
+    // 리스트를 날짜 순으로 정렬
+    schedules.sort((a, b) => a.time.compareTo(b.time));
+
+    int count = 1;
+
+    // 아이콘 로드 및 날짜별 count 초기화
+    for (int i = 0; i < schedules.length; i++) {
+      if (i > 0 &&
+          (schedules[i].time.year != schedules[i - 1].time.year ||
+              schedules[i].time.month != schedules[i - 1].time.month ||
+              schedules[i].time.day != schedules[i - 1].time.day)) {
+        count = 1; // 날짜가 바뀌면 count 초기화
+      }
+
+      // BitmapDescriptor 로드
+      final icon = await BitmapDescriptor.asset(
+        const ImageConfiguration(size: Size(40, 40)),
+        'asset/img/marker$count.png',
+      );
+
+      schedules[i].icon = icon; // 각 Schedule에 아이콘 할당
+      count++;
+    }
+
+    setState(() {}); // UI 업데이트
+  }
+
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -56,8 +98,8 @@ class _MapScreenState extends State<MapScreen> {
   // 선택된 날짜에 해당하는 Marker 필터링
   Set<Marker> getMarkers() {
     final filteredSchedules = selectedDate == null
-        ? sampleData
-        : sampleData.where((schedule) {
+        ? schedules
+        : schedules.where((schedule) {
       return schedule.time.year == selectedDate!.year &&
           schedule.time.month == selectedDate!.month &&
           schedule.time.day == selectedDate!.day;
@@ -67,6 +109,7 @@ class _MapScreenState extends State<MapScreen> {
         .map((schedule) => Marker(
       markerId: MarkerId(schedule.scheduleId.toString()),
       position: schedule.latLng,
+      icon: schedule.icon ?? BitmapDescriptor.defaultMarker, // 각 Schedule의 아이콘 사용
       infoWindow: InfoWindow(
         title: schedule.title,
         onTap: () {}, // TODO: 정보 수정
@@ -92,7 +135,7 @@ class _MapScreenState extends State<MapScreen> {
           ),
         ),
         BottomWidget(
-          datas: sampleData,
+          datas: schedules,
           onDateSelected: (DateTime? date) {
             setState(() {
               selectedDate = date;
