725399f486a4cf1363f73b3afaa832abfbd22daf
HS-JNYLee <2071013@hansung.ac.kr>
2024-12-03 11:49:50 +0900

    [ui]: schedule_view.dart item design

--- lib/screens/schedule_view.dart
+++ lib/screens/schedule_view.dart
@@ -23,88 +23,94 @@ class _ScheduleViewState extends State<ScheduleView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: const Text('📆 일정 관리',
-              style: TextStyle(fontWeight: FontWeight.w700)),
-        ),
-        body: Column(
-          children: [
-            DaySelector(
-              startDate: widget.startDate,
-              endDate: widget.endDate,
-              selectedDay: _selectedDay,
-              onSelectDay: (day) {
-                setState(() {
-                  _selectedDay = day;
-                  _selectedDate = DateTime(
-                    widget.startDate.year,
-                    widget.startDate.month,
-                    widget.startDate.day + _selectedDay - 1,
-                  );
-                });
+        title: const Text('📆 일정 관리',
+            style: TextStyle(fontWeight: FontWeight.w700)),
+      ),
+      body: Column(
+        children: [
+          DaySelector(
+            startDate: widget.startDate,
+            endDate: widget.endDate,
+            selectedDay: _selectedDay,
+            onSelectDay: (day) {
+              setState(() {
+                _selectedDay = day;
+                _selectedDate = DateTime(
+                  widget.startDate.year,
+                  widget.startDate.month,
+                  widget.startDate.day + _selectedDay - 1,
+                );
+              });
+            },
+          ),
+          Expanded(
+            child: Consumer<ScheduleModel>(
+              builder: (context, scheduleModel, child) {
+                _selectedDate ??=
+                    widget.startDate.add(Duration(days: _selectedDay - 1));
+                final filteredSchedules =
+                    scheduleModel.schedulesForDate(_selectedDate!);
+                final filteredScheduleModel = ScheduleModel();
+                filteredScheduleModel.addScheduleList(filteredSchedules);
+                return ListView.builder(
+                  itemCount: filteredScheduleModel.schedules.length,
+                  itemBuilder: (context, index) {
+                    final schedule = filteredScheduleModel.schedules[index];
+                    return Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Container(
+                          child: ScheduleItem(
+                            schedule: schedule,
+                            onEdit: () {
+                              _showScheduleForm(context, schedule: schedule);
+                            },
+                            onDelete: () {
+                              filteredScheduleModel.deleteSchedule(schedule.id);
+                              scheduleModel.deleteSchedule(schedule.id);
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: const Text('일정이 삭제되었습니다.'),
+                                  action: SnackBarAction(
+                                    label: '되돌리기',
+                                    onPressed: () {
+                                      filteredScheduleModel
+                                          .addSchedule(schedule);
+                                      scheduleModel.addSchedule(schedule);
+                                    },
+                                  ),
+                                ),
+                              );
+                            },
+                          )),
+                    );
+                  },
+                );
               },
             ),
-            Expanded(
-              child: Consumer<ScheduleModel>(
-                builder: (context, scheduleModel, child) {
-                  _selectedDate ??=
-                      widget.startDate.add(Duration(days: _selectedDay - 1));
-                  final filteredSchedules =
-                      scheduleModel.schedulesForDate(_selectedDate!);
-                  final filteredScheduleModel = ScheduleModel();
-                  filteredScheduleModel.addScheduleList(filteredSchedules);
-                  return ListView.builder(
-                    itemCount: filteredScheduleModel.schedules.length,
-                    itemBuilder: (context, index) {
-                      final schedule = filteredScheduleModel.schedules[index];
-                      return ScheduleItem(
-                        schedule: schedule,
-                        onEdit: () {
-                          _showScheduleForm(context, schedule: schedule);
-                        },
-                        onDelete: () {
-                          filteredScheduleModel.deleteSchedule(schedule.id);
-                          scheduleModel.deleteSchedule(schedule.id);
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: const Text('일정이 삭제되었습니다.'),
-                              action: SnackBarAction(
-                                label: '되돌리기',
-                                onPressed: () {
-                                  filteredScheduleModel.addSchedule(schedule);
-                                  scheduleModel.addSchedule(schedule);
-                                },
-                              ),
-                            ),
-                          );
-                        },
-                      );
-                    },
-                  );
-                },
+          ),
+        ],
+      ),
+      floatingActionButton: SizedBox(
+        width: 160,
+        height: 50,
+        child: FloatingActionButton(
+            onPressed: () => _showScheduleForm(context),
+            backgroundColor: const Color(0xff383838),
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                "일정 추가",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w800,
+                    fontSize: 16),
               ),
-            ),
-          ],
-        ),
-        floatingActionButton:
-        SizedBox(
-            width: 160,
-            height: 50,
-            child:  FloatingActionButton(
-
-                  onPressed: () => _showScheduleForm(context),
-                  backgroundColor: const Color(0xff383838),
-                  child: Container(
-                    padding: const EdgeInsets.all(10),
-                    child: const Text(
-                      "일정 추가",
-                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.w800, fontSize: 16),
-                    ),
-                  )),
-            )
-        , floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+            )),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 
--- lib/widgets/schedule_item.dart
+++ lib/widgets/schedule_item.dart
@@ -6,7 +6,8 @@ class ScheduleItem extends StatelessWidget {
   final VoidCallback onEdit;
   final VoidCallback onDelete;
 
-  ScheduleItem({
+  const ScheduleItem({
+    super.key,
     required this.schedule,
     required this.onEdit,
     required this.onDelete,
@@ -14,37 +15,42 @@ class ScheduleItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Dismissible(
-      key: Key(schedule.id),
-      direction: DismissDirection.endToStart,
-      onDismissed: (direction) {
-        onDelete();
-      },
-      background: Container(
-        alignment: Alignment.centerRight,
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        color: Colors.red,
-        child: Icon(Icons.delete, color: Colors.white),
-      ),
-      child: ListTile(
-        title: Text(
-          schedule.title,
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        subtitle: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(schedule.content),
-            Text(schedule.time),
-            Text(schedule.place),
-          ],
-        ),
-        trailing: IconButton(
-          icon: Icon(Icons.edit),
-          onPressed: onEdit,
-        ),
-      ),
-    );
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(20.0), // 둥근 모서리 적용
+        child: Dismissible(
+            key: Key(schedule.id),
+            direction: DismissDirection.endToStart,
+            onDismissed: (direction) {
+              onDelete();
+            },
+            background: Container(
+              alignment: Alignment.centerRight,
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              color: Colors.black,
+              child: const Icon(Icons.delete, color: Colors.red),
+            ),
+            child: Container(
+              child: ListTile(
+                tileColor: const Color(0xffF4F4F5),
+                title: Text(
+                  "🚩 ${schedule.title}",
+                  style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(left: 12.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("🌎 ${schedule.place}"),
+                      Text("🕔 ${schedule.time}"),
+                    ],
+                  ),
+                ),
+                trailing: IconButton(
+                  icon: const Icon(Icons.edit),
+                  onPressed: onEdit,
+                ),
+              ),
+            )));
   }
 }
-
